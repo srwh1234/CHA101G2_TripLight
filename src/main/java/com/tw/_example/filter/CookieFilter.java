60789cadb62e3fe035a20870ec852a46f9e5a987
srwh1234 <srwh1234@msn.com>
2023-06-06 21:46:26 +0800

    會員登入 使用filter判斷cookie內存的session id的測試

--- src/main/java/com/tw/_example/filter/CookieFilter.java
+++ src/main/java/com/tw/_example/filter/CookieFilter.java
@@ -2,6 +2,8 @@ package com.tw._example.filter;
 
 import java.io.IOException;
 
+import com.tw._example.login.TestLogin;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,21 +12,56 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter("*.html")
 public class CookieFilter implements Filter {
+
+	/*
+	 * 測試相關的html
+	 * http://localhost:8080/front-end/_test_login.html
+	 *
+	 * CookieFilter.java
+	 * TestLogin.java
+	 * */
 
 	@Override
 	public void doFilter(final ServletRequest request,//
 			final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 
 		// 轉成HttpServletRequest才有cookies
-		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+		final HttpServletRequest req = (HttpServletRequest) request;
+		final HttpServletResponse res = (HttpServletResponse) response;
 
-		final Cookie[] cookies = httpRequest.getCookies();
+		final String uri = req.getRequestURI();
+		final String htmlName = uri.substring(uri.lastIndexOf("/") + 1);
 
-		// 這邊可以判斷cookie夾帶的資訊
-		System.out.println("httpRequest=" + httpRequest.getRequestURL());
+		// 這邊..
+		// 正確的做法應該是@WebFilter 或其他設定方式 填入要檢驗的位置
+		if (htmlName.equals("shopping_car.html") || htmlName.equals("tickets_order.html")) {
+
+			// 這邊可以判斷cookie夾帶的資訊
+			final Cookie[] cookies = req.getCookies();
+
+			if (cookies != null) {
+				for (final Cookie cookie : cookies) {
+
+					// 我們定義的key
+					if (cookie.getName().equals(TestLogin.SESSION_ID_COOKIE_NAME)) {
+						final String validCode = cookie.getValue();
+
+						// 找看看有沒有 [驗證成功當下的session id]
+						final Object ret = request.getServletContext().getAttribute(validCode);
+
+						// 沒有的話請重新登入
+						if (ret == null) {
+							res.sendRedirect("/front-end/_test_login.html");
+							return;
+						}
+					}
+				}
+			}
+		}
 
 		// 下面一位
 		chain.doFilter(request, response);
---
+++ src/main/java/com/tw/_example/login/TestLogin.java
@@ -0,0 +1,60 @@
+package com.tw._example.login;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@CrossOrigin("*")
+@RestController
+public class TestLogin {
+
+	public static final String SESSION_ID_COOKIE_NAME = "memberId";
+
+	/*
+	 * 測試相關的html
+	 * http://localhost:8080/front-end/_test_login.html
+	 *
+	 * CookieFilter.java
+	 * TestLogin.java
+	 * */
+	@PostMapping("/testLogin")
+	public ResponseEntity<Object> testLogin(			//
+			@RequestBody final Map<String, String> map,	// 用一個Map裝客戶端的請求資料
+			final HttpServletRequest request,			//
+			final HttpServletResponse response) {		//
+
+		// 取值(json)
+		final String account = map.get("account");
+		final String password = map.get("password");
+
+		// 如果驗證成功，生成一個會話標識符並存入 cookie
+		if (account.equals("1111") && password.equals("1111")) {
+
+			// 取得驗證成功當下的session id
+			final String sessionId = request.getSession().getId();
+
+			// 存在一個共用的map裡 這邊用的是ServletContext
+			request.getServletContext().setAttribute(sessionId, "這裡可以放隨便一個物件");
+
+			// 寫cookie 並回應給客戶端
+			final Cookie cookie = new Cookie(SESSION_ID_COOKIE_NAME, sessionId);
+			cookie.setHttpOnly(true);
+			cookie.setPath("/");
+			response.addCookie(cookie);
+
+			return ResponseEntity.ok().build();
+		}
+
+		// 驗證不成功
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+}
