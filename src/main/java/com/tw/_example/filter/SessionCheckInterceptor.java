21cada73589169f5b2262a23d2fc8ee380353fff
Jay <y2sk951@gmail.com>
2023-06-06 23:50:51 +0800

    稍微整理程式碼，希望沒壞掉 呵 SessionCheckInterceptor 為 filter設定 WebMvcConfig 為設定filter作用範圍 把這兩個註解解開，則需要帳密才能進入網頁

--- src/main/java/com/tw/_example/filter/SessionCheckInterceptor.java
+++ src/main/java/com/tw/_example/filter/SessionCheckInterceptor.java
@@ -1,24 +1,24 @@
-// package com.tw._example.filter;
+//package com.tw._example.filter;
 //
-// import com.tw.member.model.Member;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import jakarta.servlet.http.HttpSession;
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.servlet.HandlerInterceptor;
+//import com.tw.member.model.Member;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
 //
-// @Component
-// public class SessionCheckInterceptor implements HandlerInterceptor {
+//@Component
+//public class SessionCheckInterceptor implements HandlerInterceptor {
 //
-// @Override
-// public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-// HttpSession session = request.getSession(false);
-// Member user = (Member) (session != null ? session.getAttribute("user") : null);
-// if (user == null) {
-// response.sendRedirect("/front-end/index.html");
-// return false;
-// } else {
-// return true;
-// }
-// }
-// }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        HttpSession session = request.getSession();
+//        Member user = (Member) (session != null ? session.getAttribute("user") : null);
+//        if (user == null) {
+//            response.sendRedirect("/front-end/index.html");
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//}
