142d4a36fe892aaf0a389fdf58ffe94cdad96ddf
Jay <y2sk951@gmail.com>
2023-06-08 15:35:08 +0800

    把濾網註解掉

--- src/main/java/com/tw/_example/filter/SessionCheckInterceptor.java
+++ src/main/java/com/tw/_example/filter/SessionCheckInterceptor.java
@@ -1,27 +1,27 @@
-package com.tw._example.filter;
-
-import com.tw.member.model.Member;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-@Component
-public class SessionCheckInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 取得當前請求的 HttpSession 物件
-        HttpSession session = request.getSession();
-        // 從會話中取得名為 "user" 的屬性，並將其轉換為 Member 物件
-        Member user = (Member) (session != null ? session.getAttribute("user") : null);
-        if (user == null) {
-            // 若使用者未登入，則將請求重新導向至登入頁面
-            response.sendRedirect("/front-end/index.html");
-            return false;
-        } else {
-            // 若使用者已登入，則允許繼續處理該請求
-            return true;
-        }
-    }
-}
+//package com.tw._example.filter;
+//
+//import com.tw.member.model.Member;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//@Component
+//public class SessionCheckInterceptor implements HandlerInterceptor {
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        // 取得當前請求的 HttpSession 物件
+//        HttpSession session = request.getSession();
+//        // 從會話中取得名為 "user" 的屬性，並將其轉換為 Member 物件
+//        Member user = (Member) (session != null ? session.getAttribute("user") : null);
+//        if (user == null) {
+//            // 若使用者未登入，則將請求重新導向至登入頁面
+//            response.sendRedirect("/front-end/index.html");
+//            return false;
+//        } else {
+//            // 若使用者已登入，則允許繼續處理該請求
+//            return true;
+//        }
+//    }
+//}
--- src/main/java/com/tw/_example/filter/WebMvcConfig.java
+++ src/main/java/com/tw/_example/filter/WebMvcConfig.java
@@ -1,29 +1,29 @@
-package com.tw._example.filter;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Autowired
-    SessionCheckInterceptor sessionCheckInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 註冊攔截器，並添加攔截路徑和排除路徑
-        registry.addInterceptor(sessionCheckInterceptor)
-                .addPathPatterns("/**/*.html") // 攔截所有以 .html 結尾的路徑
-                .excludePathPatterns("/**/index.html"); // 排除符合 **/index.html 的路徑
-    }
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 添加靜態資源處理器，指定靜態資源路徑
-        registry.addResourceHandler("/static/**") // 使用 "/static/**" 來映射靜態資源的請求。
-                // 將靜態資源的存放路徑設定為 classpath:/static/，表示靜態資源存放在類別路徑下的 static 目錄
-                .addResourceLocations("classpath:/static/");
-    }
-}
+//package com.tw._example.filter;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    SessionCheckInterceptor sessionCheckInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 註冊攔截器，並添加攔截路徑和排除路徑
+//        registry.addInterceptor(sessionCheckInterceptor)
+//                .addPathPatterns("/**/*.html") // 攔截所有以 .html 結尾的路徑
+//                .excludePathPatterns("/**/index.html"); // 排除符合 **/index.html 的路徑
+//    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // 添加靜態資源處理器，指定靜態資源路徑
+//        registry.addResourceHandler("/static/**") // 使用 "/static/**" 來映射靜態資源的請求。
+//                // 將靜態資源的存放路徑設定為 classpath:/static/，表示靜態資源存放在類別路徑下的 static 目錄
+//                .addResourceLocations("classpath:/static/");
+//    }
+//}
