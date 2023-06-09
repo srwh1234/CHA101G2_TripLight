0e74b7b50975f22b86197d2fca44066c4b65d070
LAPTOP-PATF7J21\Tibame_T14 <teieml8090@gmail.com>
2023-06-09 11:39:37 +0800

    修正一下

--- src/main/java/com/tw/contact/test/test.java
+++ src/main/java/com/tw/contact/test/test.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@SpringBootTest
 public class test {
 
     private  questReportImpl qRImpl;
