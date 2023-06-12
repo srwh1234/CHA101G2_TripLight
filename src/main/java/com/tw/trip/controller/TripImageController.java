1271c4ac98280b84c2129aa499a53bb1430477f1
Jay <y2sk951@gmail.com>
2023-06-12 19:21:16 +0800

    更新trip相片

--- src/main/java/com/tw/trip/controller/TripImageController.java
+++ src/main/java/com/tw/trip/controller/TripImageController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TripImageController {
 
+    public static String IMG_URL = "http://localhost:8080/img/trips/";
+
     private final TripService tripService;
 
     @Autowired
@@ -18,7 +20,7 @@ public class TripImageController {
     }
 
     // 用於指定 {imgUrl} 只能由數字組成。 [0-9] 表示匹配一個數字字符，+ 表示匹配前面的表達式一次或多次。
-    @GetMapping(value = "/img/trip/{imgUrl:[0-9]+}", produces = MediaType.IMAGE_GIF_VALUE)
+    @GetMapping(value = "/img/trips/{imgUrl:[0-9]+}", produces = MediaType.IMAGE_GIF_VALUE)
     public byte[] getPhoto(@PathVariable("imgUrl") final int id) {
         return tripService.findImg(id);
     }
