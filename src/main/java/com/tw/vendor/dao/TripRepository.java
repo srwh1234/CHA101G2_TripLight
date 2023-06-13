a27f67b15955d748d78f31ff13619c18560e5ecd
srwh1234 <srwh1234@msn.com>
2023-06-13 09:11:16 +0800

    先註解掉撞名檔案

--- src/main/java/com/tw/vendor/dao/TripRepository.java
+++ src/main/java/com/tw/vendor/dao/TripRepository.java
@@ -1,10 +1,10 @@
-package com.tw.vendor.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tw.vendor.model.Trip;
-
-
-@Repository
-public interface TripRepository extends JpaRepository<Trip, Integer> {}
+// package com.tw.vendor.dao;
+//
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+//
+// import com.tw.vendor.model.Trip;
+//
+//
+// @Repository
+// public interface TripRepository extends JpaRepository<Trip, Integer> {}
