aaac3203f2d3249160caed9c1ba7e1c1118ab5b2
srwh1234 <srwh1234@msn.com>
2023-06-13 15:18:33 +0800

    1111111

--- src/main/java/com/tw/ai/dto/TripDto.java
+++ src/main/java/com/tw/ai/dto/TripDto.java
@@ -1,30 +1,29 @@
 package com.tw.ai.dto;
 
-import com.tw.trip.model.Trip;
+import com.tw.trip.model.pojo.Trip;
+
 import lombok.Data;
 
 @Data
 public class TripDto {
 
-    private final int tripId;
-    private final String tripName;
-    private final int priceAdult;
-    private final String tripDescription;
-    private final int tripDay;
-    private final String city;
-    private final String tripImage;
-    private final String tripContent;;
+	private final int tripId;
+	private final String tripName;
+	private final int priceAdult;
+	private final String tripDescription;
+	private final int tripDay;
+	private final String city;
+	private final String tripImage;
+	private final String tripContent;;
 
-
-    public TripDto(final Trip trip) {
-        this.tripId = trip.getTripId();
-        this.tripName = trip.getTripName();
-        this.priceAdult = trip.getPriceAdult();
-        this.tripDescription = trip.getTripDescription();
-        this.tripDay = trip.getTripDay();
-        this.city = trip.getCity();
-        this.tripImage = trip.getImgUrlEx(0);
-        this.tripContent = trip.getTripContent();
-    }
+	public TripDto(final Trip trip) {
+		this.tripId = trip.getTripId();
+		this.tripName = trip.getTripName();
+		this.priceAdult = trip.getPriceAdult();
+		this.tripDescription = trip.getTripDescription();
+		this.tripDay = trip.getTripDay();
+		this.city = trip.getCity();
+		this.tripImage = trip.getImgUrlEx(0);
+		this.tripContent = trip.getTripContent();
+	}
 }
-
