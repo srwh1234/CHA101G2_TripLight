494464999fd83f63af40ccd41e97e2e2bb76a175
luoxiang <a91141751@gmail.com>
2023-06-13 14:02:42 +0800

    util

---
+++ src/main/java/com/tw/vendor/Util/Util2.java
@@ -0,0 +1,8 @@
+package com.tw.vendor.Util;
+
+public class Util2 {
+    public static String DRIVER="com.mysql.cj.jdbc.Driver";
+    public static String URL = "jdbc:mysql://localhost:3306/db01?serverTimezone=Asia/Taipei";
+    public static String USER_ID = "root";
+    public static String PASSWORD = "asd880826";
+}
