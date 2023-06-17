920745622e84880ea01430292434a7982348a32e
Jay <y2sk951@gmail.com>
2023-06-17 11:11:39 +0800

    修改Bug 更正信件輸出格式

--- src/main/java/com/tw/form/dto/ContactData.java
+++ src/main/java/com/tw/form/dto/ContactData.java
@@ -1,11 +1,21 @@
 package com.tw.form.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class ContactData {
     String name;
     String email;
     String phone;
     String message;
+
+    @Override
+    public String toString() {
+        return  "姓名："+ name +"\n"+
+                "電話："+ phone +"\n"+
+                "信箱："+ email +"\n"+
+                "其他需求："+ message +"\n";
+    }
 }
