ac951cef0ec03cc4054c8e2e1103b673d78b8989
Jay <y2sk951@gmail.com>
2023-06-17 15:11:59 +0800

    廠商註冊系統，添加信件系統 把commonlineRunner 註解掉

--- src/main/java/com/tw/vendor/controller/VendorController.java
+++ src/main/java/com/tw/vendor/controller/VendorController.java
@@ -2,6 +2,7 @@ package com.tw.vendor.controller;
 
 import java.util.List;
 
+import com.tw.form.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,9 @@ public class VendorController {
     @Autowired  // 注入studentRepository實例 (dao)
     private VendorService vendorService;
 
+    @Autowired
+    private EmailSenderService emailSenderService; // 寄信用
+
     @GetMapping("/vendor")  // 對應前端get,  傳資料給前端
     public List<Vendor> getveVendors() {
         return vendorService.findAll();
@@ -26,7 +30,7 @@ public class VendorController {
     @PostMapping("/vendor")
     public String processStudent(@RequestBody Vendor vendor){
         vendorService.save(vendor);
-        System.out.println(vendor);
+        emailSenderService.sendEmail("廠商申請表單",vendor.toString()); // 寄信
         return "成功拿到資料";
     }
     
--- src/main/java/com/tw/vendor/model/Vendor.java
+++ src/main/java/com/tw/vendor/model/Vendor.java
@@ -3,14 +3,12 @@ package com.tw.vendor.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.DynamicInsert;
 import java.sql.Timestamp;
 
 @Entity  // 標示該類別為實體類別，用於映射資料庫
 @Getter
 @Setter
-@ToString
 @DynamicInsert  //這個注解可以讓 Hibernate 在插入新記錄時只生成非空欄位的 SQL 語句，從而避免將 NULL 值插入 application_time 欄位。
 public class Vendor {  // 表格名稱
 	
@@ -41,6 +39,19 @@ public class Vendor {  // 表格名稱
     
     private String loginPassword;
     
-    private String vendorEmail; 
+    private String vendorEmail;
+
+    @Override
+    public String toString() {
+        return  "公司名稱："+ vendorName +"\n"+
+                "公司地址："+ vendorAdd +"\n"+
+                "公司電話："+ vendorPhone +"\n"+
+                "聯絡人姓名："+ ceoName +"\n"+
+                "聯絡人電子郵件："+ ceoEmail +"\n"+
+                "聯絡人電話："+ ceoPhone +"\n"+
+                "公司登入帳號："+ loginAccount +"\n"+
+                "公司登入密碼："+ vendorEmail +"\n"+
+                "公司信箱："+ vendorEmail +"\n";
+    }
     
 }
