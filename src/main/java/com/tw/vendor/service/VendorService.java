b4f6b59a5d60aab6750eff4a1541245f668d8696
Jay <y2sk951@gmail.com>
2023-06-19 15:59:47 +0800

    先把壞掉的程式碼註解掉 2次修改

--- src/main/java/com/tw/vendor/service/VendorService.java
+++ src/main/java/com/tw/vendor/service/VendorService.java
@@ -1,22 +1,22 @@
-//package com.tw.vendor.service;
-//
-//import java.util.List;
-//
-//import org.springframework.stereotype.Service;
-//
-//import com.tw.vendor.model.Vendor;
-//
-//@Service
-//public interface VendorService {
-//
-//    public void save(Vendor vendor);
-//
-//    public List<Vendor> findAll();
-//
-//    public void update(Vendor vendor);
-//
-//    public Vendor findById(int vendorId);
-//
-////    public void deleteById(int vendorId);
-//
-//}
+package com.tw.vendor.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tw.vendor.model.Vendor;
+
+@Service
+public interface VendorService {
+
+    public void save(Vendor vendor);
+
+    public List<Vendor> findAll();
+
+    public void update(Vendor vendor);
+
+    public Vendor findById(int vendorId);
+
+//    public void deleteById(int vendorId);
+
+}
