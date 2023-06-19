0c49e5bf1a9554de0807d051ab5cba572da11725
Jay <y2sk951@gmail.com>
2023-06-19 15:59:56 +0800

    先把壞掉的程式碼註解掉 2次修改

--- src/main/java/com/tw/vendor/service/TripService2.java
+++ src/main/java/com/tw/vendor/service/TripService2.java
@@ -1,22 +1,22 @@
-//package com.tw.vendor.service;
-//
-//import java.util.List;
-//
-//import org.springframework.stereotype.Service;
-//
-//import com.tw.vendor.model.Trip2;
-//
-//@Service
-//public interface TripService2 {
-//
-//    public void save(Trip2 trip);
-//
-//    public List<Trip2> findAll();
-//
-//    public void update(Trip2 trip);
-//
-//    public Trip2 findById(int tripId);
-//
-////    public void deleteById(int tripId);
-//
-//}
+package com.tw.vendor.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tw.vendor.model.Trip2;
+
+@Service
+public interface TripService2 {
+
+    public void save(Trip2 trip);
+
+    public List<Trip2> findAll();
+
+    public void update(Trip2 trip);
+
+    public Trip2 findById(int tripId);
+
+//    public void deleteById(int tripId);
+
+}
