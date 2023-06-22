72c6bec04c824da9b5ea4188b8669ce1d58165c6
srwh1234 <srwh1234@msn.com>
2023-06-22 22:36:09 +0800

    程式碼整理

--- src/main/java/com/tw/ticket/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tw/ticket/service/impl/OrderServiceImpl.java
@@ -22,9 +22,9 @@ import com.tw.member.model.Member;
 import com.tw.member.model.dao.MemberRepository;
 import com.tw.ticket.Config;
 import com.tw.ticket.MyUtils;
-import com.tw.ticket.controller.OrderController.OrderPageResponse;
-import com.tw.ticket.controller.OrderController.OrderRequest;
-import com.tw.ticket.controller.OrderController.OrderResponse;
+import com.tw.ticket.controller.OrderController.OrderDto;
+import com.tw.ticket.controller.OrderController.PageDto;
+import com.tw.ticket.controller.OrderController.PageReqDto;
 import com.tw.ticket.model.Coupon;
 import com.tw.ticket.model.Ticket;
 import com.tw.ticket.model.TicketCart;
@@ -73,25 +73,28 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private PromotionService promotionService;
 
-	// 訂單清單
+	/**
+	 * 獲得票券訂單清單分頁
+	 *
+	 * @param reqDto 請求參數
+	 * @return
+	 */
 	@Override
-	public OrderPageResponse getItems(final OrderRequest request) {
+	public PageDto getItem(final PageReqDto reqDto) {
 		final Pageable pageable = PageRequest.of(	//
-				request.getPage(),		// 查詢的頁數，從0起算
-				request.getSize()		// 查詢的每頁筆數
+				reqDto.getPage(),		// 查詢的頁數，從0起算
+				reqDto.getSize()		// 查詢的每頁筆數
 		);
 
-		final Page<TicketOrder> page = ticketOrderRepository.findByMemberId(//
-				request.getMemberId(),	// 會員編號
-				pageable				// 分頁
-		);
+		final Page<TicketOrder> page = //
+				ticketOrderRepository.findByMemberId(reqDto.getMemberId(), pageable);
 
 		// 轉成自己定義的物件
-		final OrderPageResponse pageResponse = new OrderPageResponse();
-		pageResponse.setCurPage(request.getPage());
-		pageResponse.setTotalPage(page.getTotalPages());
+		final PageDto pageDto = new PageDto();
+		pageDto.setCurPage(reqDto.getPage());
+		pageDto.setTotalPage(page.getTotalPages());
 
-		page.getContent().forEach(order -> {
+		for (final TicketOrder order : page.getContent()) {
 			int couponId = 0;
 			String couponName = null;
 			if (order.getCoupon() != null) {
@@ -99,21 +102,138 @@ public class OrderServiceImpl implements OrderService {
 				couponName = order.getCoupon().getName();
 			}
 
-			final OrderResponse response = new OrderResponse();
-			response.setTicketOrderId(order.getTicketOrderId());
-			response.setTicketCount(order.getTicketOrderDetails().size());
-			response.setCouponId(couponId);
-			response.setCouponName(couponName);
-			response.setPayDate(order.getPayDate());
-			response.setPayType(order.getPayType());
-			response.setActualPrice(order.getActualPrice());
-			pageResponse.getOrders().add(response);
-		});
-
-		return pageResponse;
+			final OrderDto orderDto = new OrderDto();
+			orderDto.setTicketOrderId(order.getTicketOrderId());
+			orderDto.setTicketCount(order.getTicketOrderDetails().size());
+			orderDto.setCouponId(couponId);
+			orderDto.setCouponName(couponName);
+			orderDto.setPayDate(order.getPayDate());
+			orderDto.setPayType(order.getPayType());
+			orderDto.setActualPrice(order.getActualPrice());
+			pageDto.getOrders().add(orderDto);
+		}
+		return pageDto;
 	}
 
-	// 建立訂單
+	/**
+	 * 綠界支付介面 設定後要丟給前端的資料(新增訂單)
+	 *
+	 * @param map 請求參數
+	 * @return
+	 */
+	@Override
+	public String ecpayFromCarts(final Map<String, Object> map) {
+		final int memberId = (int) map.get("memberId");
+		final int couponId = (int) map.get("couponId");
+		final String backUrl = (String) map.get("backUrl");
+
+		// 建立訂單
+		final TicketOrder order = makeOrder(memberId, couponId);
+
+		if (order == null) {
+			return null;
+		}
+		return getAllInOnePage(memberId, order, backUrl);
+	}
+
+	/**
+	 * 綠界支付介面 設定後要丟給前端的資料(現有訂單)
+	 *
+	 * @param map 請求參數
+	 * @return
+	 */
+	@Override
+	public String ecpayFromOrder(final Map<String, Object> map) {
+		final int memberId = (int) map.get("memberId");
+		final int orderId = (int) map.get("orderId");
+		final String backUrl = (String) map.get("backUrl");
+		final TicketOrder order = ticketOrderRepository.findById(orderId).orElse(null);
+		if (order == null) {
+			return null;
+		}
+		return getAllInOnePage(memberId, order, backUrl);
+	}
+
+	/**
+	 * 綠界支付的結果 如果結帳成功就完成訂單
+	 *
+	 * @param memberId 會員編號
+	 * @param orderId 訂單編號
+	 * @param payType 付款類型
+	 */
+	@Override
+	public void ecpayConfirm(final int memberId, final int orderId, final String payType) {
+		final TicketOrder order = ticketOrderRepository.findById(orderId).orElse(null);
+		if (order == null) {
+			return;
+		}
+		confirmOrder(order.getTicketOrderId(), payType);
+	}
+
+	/**
+	 * 測試用結帳
+	 *
+	 * @param memberId 會員編號
+	 * @param couponId 優惠券編號
+	 * @return
+	 */
+	@Override
+	public boolean testOrder(final int memberId, final int couponId) {
+		final TicketOrder order = makeOrder(memberId, couponId);
+
+		if (order == null) {
+			return false;
+		}
+		confirmOrder(order.getTicketOrderId(), "TEST");
+		return true;
+	}
+
+	// --------------------------------------------
+
+	/**
+	 * 綠界付款介面的初始化
+	 *
+	 * @param memberId 會員編號
+	 * @param order 訂單
+	 * @param backUrl 返回的頁面路徑
+	 * @return
+	 */
+	private String getAllInOnePage(final int memberId, final TicketOrder order, final String backUrl) {
+		int couponId = 0;
+		String itemName = "TripLight訂單";
+
+		final Coupon coupon = order.getCoupon();
+		if (coupon != null) {
+			couponId = coupon.getCouponId();
+			itemName += String.format(" (%s)", coupon.getName());
+		}
+		final String uuId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
+
+		final AioCheckOutALL obj = new AioCheckOutALL();
+		obj.setMerchantTradeNo(uuId);
+		obj.setMerchantTradeDate(MyUtils.getNowDateTimeString());
+		obj.setTotalAmount(String.valueOf(order.getActualPrice()));
+		obj.setTradeDesc("test Description");
+		obj.setItemName(itemName);
+		obj.setReturnURL(config.getEcpayReturnUrl() + "/callback");
+		obj.setClientBackURL(backUrl);
+
+		// 參數傳遞
+		obj.setCustomField1(String.valueOf(memberId));
+		obj.setCustomField2(String.valueOf(couponId));
+		obj.setCustomField3(String.valueOf(order.getTicketOrderId()));
+		obj.setNeedExtraPaidInfo("Y");
+
+		return new AllInOne("").aioCheckOut(obj, null);
+	}
+
+	/**
+	 * 建立訂單
+	 *
+	 * @param memberId 會員編號
+	 * @param couponId 優惠券編號
+	 * @return
+	 */
 	private TicketOrder makeOrder(final int memberId, final int couponId) {
 		// 確認會員
 		final Member member = memberRepository.findById(memberId).orElse(null);
@@ -125,7 +245,6 @@ public class OrderServiceImpl implements OrderService {
 		// 總價與實際價格
 		int totalPrice = 0;
 		int actualPrice = 0;
-
 		final HashMap<Integer, List<TicketSn>> temps = new HashMap<>();
 
 		// 會員購物車
@@ -134,7 +253,6 @@ public class OrderServiceImpl implements OrderService {
 		if (ticketCarts.size() <= 0) {
 			return null;
 		}
-
 		for (final TicketCart cart : ticketCarts) {
 			if (cart.getQuantity() <= 0) {
 				continue;
@@ -152,7 +270,6 @@ public class OrderServiceImpl implements OrderService {
 
 			// 促銷
 			final int price = promotionService.getPrice(ticket);
-
 			totalPrice += price * cart.getQuantity();
 		}
 
@@ -181,6 +298,7 @@ public class OrderServiceImpl implements OrderService {
 
 		// 訂單明細的品項都要有獨一的序號
 		final List<TicketOrderDetail> orderDetails = new ArrayList<>();
+
 		for (final TicketCart cart : ticketCarts) {
 			final List<TicketSn> ticketSns = temps.get(cart.getTicketId());
 
@@ -243,85 +361,5 @@ public class OrderServiceImpl implements OrderService {
 		// 更新票券的銷售量
 		ticketRepository.saveAll(tickets);
 		return true;
-	}
-
-	// 成立訂單(test)
-	@Override
-	public boolean testOrder(final int memberId, final int couponId) {
-		final TicketOrder order = makeOrder(memberId, couponId);
-
-		if (order == null) {
-			return false;
-		}
-		confirmOrder(order.getTicketOrderId(), "TEST");
-		return true;
-	}
-
-	// 綠界的支付結果
-	@Override
-	public void ecpayConfirm(final int memberId, final int orderId, final String payType) {
-		final TicketOrder order = ticketOrderRepository.findById(orderId).orElse(null);
-		if (order == null) {
-			return;
-		}
-		confirmOrder(order.getTicketOrderId(), payType);
-	}
-
-	// XXX 綠界的支付介面設定(現有訂單)
-	@Override
-	public String ecpayCheckoutOrder(final Map<String, Object> map) {
-		final int memberId = (int) map.get("memberId");
-		final int orderId = (int) map.get("orderId");
-		final String backUrl = (String) map.get("backUrl");
-		final TicketOrder order = ticketOrderRepository.findById(orderId).orElse(null);
-		if (order == null) {
-			return null;
-		}
-		return getAllInOnePage(memberId, order, backUrl);
-	}
-
-	// 綠界的支付介面設定(新增訂單)
-	@Override
-	public String ecpayCheckoutMake(final Map<String, Object> map) {
-		final int memberId = (int) map.get("memberId");
-		final int couponId = (int) map.get("couponId");
-		final String backUrl = (String) map.get("backUrl");
-		// 建立訂單
-		final TicketOrder order = makeOrder(memberId, couponId);
-
-		if (order == null) {
-			return null;
-		}
-
-		return getAllInOnePage(memberId, order, backUrl);
-	}
-
-	private String getAllInOnePage(final int memberId, final TicketOrder order, final String backUrl) {
-		int couponId = 0;
-		String itemName = "TripLight訂單";
-
-		final Coupon coupon = order.getCoupon();
-		if (coupon != null) {
-			couponId = coupon.getCouponId();
-			itemName += String.format(" (%s)", coupon.getName());
-		}
-		final String uuId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
-
-		final AioCheckOutALL obj = new AioCheckOutALL();
-		obj.setMerchantTradeNo(uuId);
-		obj.setMerchantTradeDate(MyUtils.getNowDateTimeString());
-		obj.setTotalAmount(String.valueOf(order.getActualPrice()));
-		obj.setTradeDesc("test Description");
-		obj.setItemName("TripLight訂單###");
-		obj.setReturnURL(config.getEcpayReturnUrl() + "/callback");
-		obj.setClientBackURL(backUrl);
-
-		// 參數傳遞
-		obj.setCustomField1(String.valueOf(memberId));
-		obj.setCustomField2(String.valueOf(couponId));
-		obj.setCustomField3(String.valueOf(order.getTicketOrderId()));
-		obj.setNeedExtraPaidInfo("Y");
-
-		return new AllInOne("").aioCheckOut(obj, null);
 	}
 }
