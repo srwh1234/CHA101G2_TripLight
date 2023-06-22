1653b802d3b79d1000423d964f44a7e7d9d1236a
Jay <y2sk951@gmail.com>
2023-06-22 10:46:12 +0800

    新增HTML格式轉換API HTMLFormat.getHTMLFormat(String title,String titleColor,String content) HTMLFormat.getHTMLFormat(String title,String titleColor,String content,String ButtonTitle,String ButtonLink) 要斷行自己加<br>

--- src/main/java/com/tw/vendor/controller/VendorController.java
+++ src/main/java/com/tw/vendor/controller/VendorController.java
@@ -3,6 +3,7 @@ package com.tw.vendor.controller;
 import java.util.List;
 
 import com.tw.form.service.EmailSenderService;
+import com.tw.form.util.HTMLFormat;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,8 @@ public class VendorController {
     public String processVendor(@RequestBody Vendor vendor){
     	System.out.println("Get");
         vendorService.save(vendor);
-        emailSenderService.sendEmail("triplight0411@gmail.com","廠商申請表單",vendor.toString()); // 寄信
+        String htmlFormat = HTMLFormat.getHTMLFormat("TripLight廠商申請表單", "blue", vendor.toString(),"前往後臺審核!", "http://localhost:8080/back-end/index.html");
+        emailSenderService.sendHTMLEmail("triplight0411@gmail.com","廠商申請表單",htmlFormat); // 寄信
         return "成功拿到資料";
     }
 
@@ -38,96 +40,10 @@ public class VendorController {
         Vendor vendor = vendorService.findById(vendorId);
         vendor.setReview(1);
         vendorService.save(vendor);
-        String htmlContent = """
-                <!DOCTYPE html>
-                        <html>
-                        
-                        <head>
-                            <style>
-                                body {
-                                    font-family: Arial, sans-serif;
-                                    background-color: #F5F5F5;
-                                }
-                        
-                                .container {
-                                    max-width: 800px;
-                                    margin: 0 auto;
-                                    padding: 30px 20px;
-                                    background-color: #fafafa;
-                                    border-radius: 15px;
-                                    border: 1px solid #E8E8E8;
-                                }
-                        
-                                h1 {
-                                    color: #5C6BC0;
-                                    text-align: center;
-                                    margin-top: 0;
-                                    font-size: 2.5em;
-                        
-                                }
-                        
-                                p {
-                                    font-size: 1.2em;
-                                    font-weight: 400;
-                                    color: #333;
-                                    margin-bottom: 10px;
-                                    line-height: 1.6;
-                                }
-                        
-                                .info {
-                                    margin-top: 30px;
-                                    padding: 10px;
-                                    border: 1px solid #BDBDBD;
-                                    border-radius: 10px;
-                                    background-color: #ECEFF1;
-                                    text-align: center;
-                                }
-                        
-                                .button-container {
-                                    text-align: center;
-                                    margin-top: 30px;
-                                }
-                        
-                                .button {
-                                    display: inline-block;
-                                    padding: 12px 24px;
-                                    background-color: #5C6BC0;
-                                    color: #ffffff !important;
-                                    text-decoration: none;
-                                    border-radius: 5px;
-                                    font-weight: 700;
-                                    font-size: 1.2em;
-                                    text-align: center;
-                                    transition: background-color 0.3s ease;
-                                }
-                        
-                                .button:hover {
-                                    background-color: #3949AB;
-                                }
-                            </style>
-                        </head>
-                        
-                        <body>
-                            <div class="container">
-                                <h1>恭喜TripLight廠商帳號啟用成功</h1>
-                        
-                                <div class="info">
-                                    <p>您的註冊email為：%s</p>
-                                    <p>您的帳號為：%s</p>
-                                    <p>您的密碼為：%s</p>
-                                </div>
-                        
-                                <div class="button-container">
-                                    <a href="http://localhost:8080/vendor-end/login.html" class="button">前往登入</a>
-                                </div>
-                            </div>
-                        </body>
-                        
-                        </html>
-                        
-                """;
-        String formattedMessage = String.format(htmlContent,vendor.getVendorEmail() ,vendor.getLoginAccount(), vendor.getLoginPassword());
-        emailSenderService.sendHTMLEmail(vendor.getVendorEmail(),"恭喜TripLight廠商帳號啟用成功",formattedMessage);
+        String message = "您的註冊email為：%s<br>您的帳號為：%s<br>您的密碼為：%s";
+        String content = String.format(message,vendor.getVendorEmail() ,vendor.getLoginAccount(), vendor.getLoginPassword());
+        String htmlFormat = HTMLFormat.getHTMLFormat("恭喜TripLight廠商帳號啟用成功", "green", content, "前往登入!", "http://localhost:8080/vendor-end/login.html");
+        emailSenderService.sendHTMLEmail(vendor.getVendorEmail(),"恭喜TripLight廠商帳號啟用成功",htmlFormat);
         return "廠商啟用成功";
     }
     // 更改狀態- 停權
@@ -136,99 +52,15 @@ public class VendorController {
         Vendor vendor = vendorService.findById(vendorId);
         vendor.setReview(2);
         vendorService.save(vendor);
-        String htmlContent = """
-                <!DOCTYPE html>
-                <html>
-                               
-                <head>
-                    <style>
-                        body {
-                            font-family: Arial, sans-serif;
-                            background-color: #F5F5F5;
-                        }
-                               
-                        .container {
-                            max-width: 800px;
-                            margin: 0 auto;
-                            padding: 30px 20px;
-                            background-color: #fafafa;
-                            border-radius: 15px;
-                            border: 1px solid #E8E8E8;
-                        }
-                               
-                        h1 {
-                            color: #ca0f0f;
-                            text-align: center;
-                            margin-top: 0;
-                            font-size: 2.5em;
-                               
-                        }
-                               
-                        p {
-                            font-size: 1.2em;
-                            font-weight: 400;
-                            color: #333;
-                            margin-bottom: 10px;
-                            line-height: 1.6;
-                        }
-                               
-                        .info {
-                            margin-top: 30px;
-                            padding: 10px;
-                            border: 1px solid #BDBDBD;
-                            border-radius: 10px;
-                            background-color: #ECEFF1;
-                            text-align: center;
-                        }
-                               
-                        .button-container {
-                            text-align: center;
-                            margin-top: 30px;
-                        }
-                               
-                        .button {
-                            display: inline-block;
-                            padding: 12px 24px;
-                            background-color: #5C6BC0;
-                            color: #ffffff !important;
-                            text-decoration: none;
-                            border-radius: 5px;
-                            font-weight: 700;
-                            font-size: 1.2em;
-                            text-align: center;
-                            transition: background-color 0.3s ease;
-                        }
-                               
-                        .button:hover {
-                            background-color: #3949AB;
-                        }
-                    </style>
-                </head>
-                               
-                <body>
-                    <div class="container">
-                        <h1>TripLight廠商帳號停權通知</h1>
-                               
-                        <div class="info">
-                            <p>您的廠商帳號已被停權，如有問題請聯繫客服</p>
-                            <p>客服電話：0987987987</p>
-                        </div>
-                               
-                        <div class="button-container">
-                            <a href="http://localhost:8080/front-end/index.html" class="button">聯繫客服</a>
-                        </div>
-                    </div>
-                </body>
-                               
-                </html>
-                        
-                """;
-
-        // TODO:修正
-        String formattedMessage = String.format(htmlContent,vendor.getVendorEmail() ,vendor.getLoginAccount(), vendor.getLoginPassword());
-        emailSenderService.sendHTMLEmail(vendor.getVendorEmail(),"TripLight廠商帳號停權通知",formattedMessage);
+        String content = "您的廠商帳號已被停權，如有問題請聯繫客服<br>客服電話：0987987987";
+        String htmlFormat = HTMLFormat.getHTMLFormat("TripLight廠商帳號停權通知", "red", content, "聯繫客服", "http://localhost:8080/front-end/index.html");
+        emailSenderService.sendHTMLEmail(vendor.getVendorEmail(),"TripLight廠商帳號停權通知",htmlFormat);
         return "廠商停權成功";
     }
+
+
+
+
 
 
 
