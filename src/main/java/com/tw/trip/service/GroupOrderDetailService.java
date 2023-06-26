a177d5a6c23e5a1291073106d2ebabc38fef6818
Victoria Chang <donut2012summer@gmail.com>
2023-06-26 14:01:35 +0800

    update group order, detail n search

--- src/main/java/com/tw/trip/service/GroupOrderDetailService.java
+++ src/main/java/com/tw/trip/service/GroupOrderDetailService.java
@@ -1,11 +1,19 @@
 package com.tw.trip.service;
 
+import com.tw.member.model.Member;
+import com.tw.trip.dao.TripDao;
+import com.tw.trip.pojo.TourGroup;
+import com.tw.trip.pojo.Trip;
 import com.tw.trip.pojo.TripOrder;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -14,6 +22,9 @@ public class GroupOrderDetailService {
 
     @PersistenceContext
     Session session;
+
+    @Autowired
+    TripDao tripDao;
 
     public List<TripOrder> getOrderInfor(Integer tripOrderId){
         final String HQL = """
@@ -27,7 +38,55 @@ public class GroupOrderDetailService {
         return resultList ;
     }
 
+    public Member getMemberInfor(Integer memberId){
+        final String HQL = """
+                SELECT new com.tw.member.model.Member(memberNameLast, memberNameFirst, memberPhone) FROM Member
+                WHERE memberId= :memberId
+                """;
+        Member member = session.createQuery(HQL,Member.class)
+                .setParameter("memberId", memberId)
+                .uniqueResult();
+
+        return member;
+
+    }
+
+    public TourGroup getTourGroupDate(Integer tripId, Integer tourGroupId) {
+
+        final String HQL = """
+                SELECT new com.tw.trip.pojo.TourGroup(startDate) FROM TourGroup
+                WHERE tourGroupId = :tourGroupId
+                """;
+
+        TourGroup tourGroup = session.createQuery(HQL, TourGroup.class)
+                .setParameter("tourGroupId", tourGroupId)
+                .uniqueResult();
+
+
+        // get tripDay from trip
+        Trip trip = tripDao.findByPrimaryKey(tripId);
+        Integer tripDay = trip.getTripDay();
+        String tripName = trip.getTripName();
+
+        // get startDate from tour_group
+        LocalDate startDate = tourGroup.getStartDate().toLocalDate();
+
+        // calculate days
+        LocalDate calculatedDate = startDate.plusDays(tripDay);
+
+        // format days
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedStartDate = startDate.format(formatter);
+        String formattedEndDate = calculatedDate.format(formatter);
+
+        // set and return
+        tourGroup.setFormattedStartDate(formattedStartDate);
+        tourGroup.setFormattedEndDate(formattedEndDate);
+        tourGroup.setTripName(tripName);
+        return tourGroup;
+
+       }
 
 
 
-}
+    }
