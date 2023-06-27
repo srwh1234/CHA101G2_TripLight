28aa069f629ed927d6357b2e72c5cff36aadd149
Maple <go02849854@Gmail.com>
2023-06-27 16:10:27 +0800

    更新

--- src/main/java/com/tw/employee/controller/CustomWebMvcConfigurer.java
+++ src/main/java/com/tw/employee/controller/CustomWebMvcConfigurer.java
@@ -10,7 +10,7 @@ public class CustomWebMvcConfigurer implements WebMvcConfigurer {
 	  @Override
 	    public void addInterceptors(InterceptorRegistry registry) {
 	        registry.addInterceptor(new BKloginIntercetor())
-	        		.addPathPatterns("/**/*.html") // 指定攔截的路徑只攔截html
+	        		.addPathPatterns("/back-end/*.html") // 指定攔截的路徑只攔截html
 	                .excludePathPatterns("/back-end/login.html"); // 排除登入頁面的路徑
 	    }
 }
