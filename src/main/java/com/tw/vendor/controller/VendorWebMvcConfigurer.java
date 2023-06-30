245405ea7c7cbd38be005abd0f78f09fb543b31e
luoxiang <a91141751@gmail.com>
2023-06-30 19:58:51 +0800

    除錯

--- src/main/java/com/tw/vendor/controller/VendorWebMvcConfigurer.java
+++ src/main/java/com/tw/vendor/controller/VendorWebMvcConfigurer.java
@@ -2,10 +2,11 @@ package com.tw.vendor.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class VendorWebMvcConfigurer {
+public class VendorWebMvcConfigurer implements WebMvcConfigurer {
 	
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new BKVendorLoginIntercetor()) //註冊要使用的類別
