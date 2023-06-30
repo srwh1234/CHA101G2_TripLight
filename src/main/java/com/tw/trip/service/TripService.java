bc59d152b040dc41d88d8d951638a7bc2e49c828
Jay <y2sk951@gmail.com>
2023-06-30 19:29:16 +0800

    更新 旅遊網頁

--- src/main/java/com/tw/trip/service/TripService.java
+++ src/main/java/com/tw/trip/service/TripService.java
@@ -7,6 +7,8 @@ import com.tw.trip.pojo.Trip;
 import com.tw.trip.pojo.TripImage;
 import com.tw.trip.repository.TripRepository;
 import com.tw.trip.repository.TripImageRepository;
+import com.tw.vendor.dao.TripRepository2;
+import com.tw.vendor.model.Trip2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,12 +19,12 @@ import java.util.List;
 
 @Service
 public class TripService{
-    private final TripRepository tripRepository;
+    private final TripRepository2 tripRepository2;
     private final TripImageRepository tripImageRepository;
 
     @Autowired
-    public TripService(TripRepository tripRepository, TripImageRepository tripImageRepository) {
-        this.tripRepository = tripRepository;
+    public TripService(TripRepository2 tripRepository2, TripImageRepository tripImageRepository) {
+        this.tripRepository2 = tripRepository2;
         this.tripImageRepository = tripImageRepository;
     }
 
@@ -32,7 +34,7 @@ public class TripService{
 
         // Pageable 是 Spring Data 提供的一個介面，用於支持分頁和排序的查詢操作。
         Pageable pageable = PageRequest.of(0, 8); // 設置只返回前8個結果
-        tripRepository.findAllByOrderByTotalSalesDesc(pageable).forEach(trip -> tripDtoArrayList.add(new TripDto(trip)));
+        tripRepository2.findAllByOrderByTotalSalesDesc(pageable).forEach(trip -> tripDtoArrayList.add(new TripDto(trip)));
         return tripDtoArrayList;
     }
 
@@ -47,21 +49,21 @@ public class TripService{
     // 提供地點名稱，獲得包含該地點名稱的行程
     public List<TripDto> getTrip(String destination){
         var tripDtoArrayList = new ArrayList<TripDto>();
-        tripRepository.findByCityContaining(destination).forEach(trip -> tripDtoArrayList.add(new TripDto(trip)));
+        tripRepository2.findByCityContaining(destination).forEach(trip -> tripDtoArrayList.add(new TripDto(trip)));
         return tripDtoArrayList;
     }
 
 
     // 提供id 獲得trip
-    public Trip findById(int tripId){
-        return tripRepository.findById(tripId).orElse(null);
+    public Trip2 findById(int tripId){
+        return tripRepository2.findById(tripId).orElse(null);
     }
 
-    public void save(Trip trip){
-        tripRepository.save(trip);
+    public void save(Trip2 trip){
+        tripRepository2.save(trip);
     }
 
-    public List<Trip> findAll(){
-        return tripRepository.findAll();
+    public List<Trip2> findAll(){
+        return tripRepository2.findAll();
     }
 }
