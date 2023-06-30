1ee3c8aa0c2d2e86608509ae4671abe2fc1f87ff
LAPTOP-PATF7J21\Tibame_T14 <teieml8090@gmail.com>
2023-06-30 16:16:04 +0800

    update

---
+++ src/main/java/com/tw/contact/testChat/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.tw.contact.testChat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
