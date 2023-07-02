5e7516cdbdef50582eb3387a0c274d62ff856ccc
Teim <Teieml8090@gmail.com>
2023-07-02 20:41:31 +0800

    刪除一些拉機

--- src/main/java/com/tw/contact/controller/QuestionReportController.java
+++ src/main/java/com/tw/contact/controller/QuestionReportController.java
@@ -21,24 +21,25 @@ public class QuestionReportController {
     }
 
     /*
-    * 前台處理
+     * 前台處理
      */
 
     //會員送出問題
     @PostMapping(value = "/questionReportForm")
-    public ResponseEntity<String> save(@RequestBody QuestionReportRequestDTO questionReportRequestDTO){
+    public ResponseEntity<String> save(@RequestBody QuestionReportRequestDTO questionReportRequestDTO) {
         questionReportService.createQuestionReport(questionReportRequestDTO);
         return ResponseEntity.ok().body("{\"message\": \"問題已送出!\"}");
     }
+
     //接收會員的評分
-    @PostMapping(value ="/questionReportForm/score/{id}")
-    public ResponseEntity<String> updateScore(@PathVariable int id, @RequestBody Map<String, Integer> scoreMap){
+    @PostMapping(value = "/questionReportForm/score/{id}")
+    public ResponseEntity<String> updateScore(@PathVariable int id, @RequestBody Map<String, Integer> scoreMap) {
         questionReportService.updateScore(id, scoreMap.get("score"));
         return ResponseEntity.ok().body("{\"message\": \"評分已送出!\"}");
     }
 
     @GetMapping(value = "/questionReportForm/{memberId}")
-    public List<QuestionReport> showMemberQuestionList(@PathVariable int memberId){
+    public List<QuestionReport> showMemberQuestionList(@PathVariable int memberId) {
         return questionReportService.showQuestionReportById(memberId);
     }
 
@@ -50,27 +51,28 @@ public class QuestionReportController {
 
     //會員查看詳細回覆內容
     @GetMapping(value = "/questionReportFormDetail")
-    public QuestionReport showQuestionDetail(@RequestParam int id){
+    public QuestionReport showQuestionDetail(@RequestParam int id) {
         return questionReportService.checkQuestionDetail(id);
     }
 
-    
+
     /*
     後臺處理
      */
     @GetMapping(value = "/questionReportForm")
-    public List<QuestionReport> showQuestion(){
+    public List<QuestionReport> showQuestion() {
         return questionReportService.showQuestionReportByState(0);
     }
 
     @PutMapping("/questionReportForm/{id}")
     public ResponseEntity<String> handleQuestionReport(@PathVariable int id, @RequestBody Map<String, Object> payload) {
-        String replyContent = payload.get("replyContent").toString();
-        int employeeId = Integer.parseInt(payload.get("employeeId").toString());
+//        String replyContent = payload.get("replyContent").toString();
+        String replyContent = (String) payload.get("replyContent");
+//        int employeeId = Integer.parseInt(payload.get("employeeId").toString());
+        int employeeId = (int) payload.get("employeeId");
         questionReportService.updateQuestionReport(id, employeeId, replyContent);
         return ResponseEntity.ok().body("{\"message\": \"已處理問題!\"}");
     }
-
 
 
 }
