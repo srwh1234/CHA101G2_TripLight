a509f6e0d8343030e4a90e46cbab41684ffbde3c
Jay <y2sk951@gmail.com>
2023-07-03 19:06:36 +0800

    初步修改 chatGPT API

--- src/main/java/com/tw/ai/util/GetLocation.java
+++ src/main/java/com/tw/ai/util/GetLocation.java
@@ -1,5 +1,6 @@
 package com.tw.ai.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -8,12 +9,11 @@ import com.tw.ai.dto.AiLocationsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,6 +33,8 @@ public class GetLocation {
 
     private final String API_KEY;
     private static final String MODEL = "gpt-3.5-turbo";
+
+    private static final int MAX_TOKENS = 2500; // 設定每次請求的字數上限 (一個中文字2Token, 一個英文字1Token)
     private static final String URL = "https://api.openai.com/v1/chat/completions";
 
     @Autowired
@@ -40,34 +42,97 @@ public class GetLocation {
         this.API_KEY = appConfig.getApiKey();
     }
 
-    // 這個方法會傳回一個 InputStream，該 InputStream 會連線到 ChatGPT API 並傳送 message
-    private BufferedReader chatGPT(String message) throws IOException {
+    //  ======================================== 串接API ========================================
+    // 建立連線，回傳BufferedReader
+    public BufferedReader chatGPT(String message) throws IOException {
+        // 創建json請求主體
+        String data = createJSONPayload(message);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode rootNode = objectMapper.createObjectNode();
-        rootNode.put("model", MODEL);
-        ArrayNode messagesNode = rootNode.putArray("messages");
-        ObjectNode messageNode = messagesNode.addObject();
-        messageNode.put("role", "user");
-        messageNode.put("content", message);
-        rootNode.put("max_tokens", 1000);
-        rootNode.put("stream", true);
-        String data = objectMapper.writeValueAsString(rootNode);
+        // 建立chatGPT 連線
+        HttpURLConnection connection = createConnection();
 
-        // 連線到 ChatGPT API
-        java.net.URL url = new URL(URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("Authorization", "Bearer " + API_KEY);
-        connection.setDoOutput(true);
-        OutputStream out = connection.getOutputStream();
-        out.write(data.getBytes());
-        out.flush();
+        // 發送請求
+        sendRequest(connection, data);
 
-        // 取得回傳的 BufferedReader
-        return new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        // 獲取回應
+        return getResponse(connection);
     }
+
+
+    // 創建json 請求主體
+    private String createJSONPayload(String message) throws JsonProcessingException, UnsupportedEncodingException {
+        // 建立Object mapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 使用 objectMapper 建立一個 ObjectNode
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        // 添加屬性, 分別為模型設定， token 數量（max_tokens），是否進行流傳輸（stream）
+        objectNode.put("model", MODEL)
+                .put("max_tokens", MAX_TOKENS)
+                .put("stream", true);
+
+
+
+        // 添加陣列
+        objectNode.putArray("messages").addObject()
+                .put("role", "user")
+                .put("content", message);
+
+        // 使用 objectMapper 將 ObjectNode 轉換為字串
+        return objectMapper.writeValueAsString(objectNode);
+    }
+
+    // 建立連線
+    private HttpURLConnection createConnection() throws IOException {
+        // 建立url連線
+        java.net.URL url = new URL(URL);
+
+        // 建立與指定 URL 之間的連線並返回一個 URLConnection 物件
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // 設定連線的請求方法
+        connection.setRequestMethod("POST");
+
+        // 設定連線的請求屬性
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization", "Bearer " + API_KEY); // 注意Bearer 後面的空格
+
+        // 設定連線是否可以向伺服器寫入資料
+        connection.setDoOutput(true);
+
+        // 回傳連線
+        return connection;
+    }
+
+    // 發送請求
+    private void sendRequest(HttpURLConnection connection, String data) throws IOException {
+        // 獲取與連線相關的輸出流
+        OutputStream outputStream = connection.getOutputStream();
+
+        // 將字串轉為位元，並將位元資料寫入輸出流
+        outputStream.write(data.getBytes());
+
+        // 強制將緩衝區資料寫入
+        outputStream.flush();
+    }
+
+    // 獲取響應
+    public BufferedReader getResponse(HttpURLConnection connection) throws IOException {
+        // 獲取與連線相關輸入流，這是從伺服器接收資料的通道
+        InputStream inputStream = connection.getInputStream();
+
+        // 將位元輸入流轉為字元輸入流，使用 UTF-8 編碼
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+        // 將字元輸入流添加緩衝，減少I/O次數
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        // 回傳輸入流，給呼叫的method自行處理
+        return bufferedReader;
+    }
+
+    //  ======================================== 串接API ========================================
 
 
     // 以下是各地點的經緯度：
