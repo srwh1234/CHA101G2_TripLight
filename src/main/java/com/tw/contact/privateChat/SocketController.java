45d1331c08c049771fb94d60292732cc84e464e9
Teim <Teieml8090@gmail.com>
2023-07-04 23:33:25 +0800

    update

--- src/main/java/com/tw/contact/privateChat/SocketController.java
+++ src/main/java/com/tw/contact/privateChat/SocketController.java
@@ -1,5 +1,7 @@
 package com.tw.contact.privateChat;
 
+
+import com.tw.employee.dao.EmployeeRepository;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
@@ -12,206 +14,183 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 //参数role判断用户角色0是客服，1是用户
-@ServerEndpoint(value = "/websocket/{role}")
+@ServerEndpoint(value = "/websocket/{role}/{userId}")
 @Component
 public class SocketController {
 
     //用本地執行緒保存session
     private static ThreadLocal<Session> sessions = new ThreadLocal<Session>();
-    //保存上線客戶 String放SessionID
+    //保存上線用戶 String放SessionID
     private static Map<String, SocketUserInfo> userSessionMap = new ConcurrentHashMap<>();
     //保存上線客服 String放SessionID
     private static Map<String, SocketUserInfo> serverSessionMap = new ConcurrentHashMap<>();
 
     //連線設定
     @OnOpen
-    public void onOpen(Session session, @PathParam(value="role") Integer role) {
-
-        //設定狀態map
-        Map<String, String> state = new HashMap<>();
+    public void onOpen(Session session, @PathParam(value = "role") int role, @PathParam(value = "userId") int userId) {
+        //用來儲存訊息
         Map<String, String> msg = new HashMap<>();
-        //預設連線失敗
-        state.put("state", "連線失敗");
 
         //執行緒獨立
         sessions.set(session);
 
         //客服上線的情況
-        if (role.equals(0)) {
+        if (role == 0) {
             //建立一個客服資訊
+            EmployeeRepository employeeRepository = SpringApplicationContext.getBean(EmployeeRepository.class);
             SocketUserInfo serverInfo = new SocketUserInfo();
+            serverInfo.setName(employeeRepository.findByEmployeeId(userId).getEmployeeName());
             serverInfo.setSessionId(session.getId());
             serverInfo.setSession(session);
             serverInfo.setUserRole("客服");
 
-            //客服連線狀態為成功
-            state.put("state", "success");
-
-            //查詢是否有客戶正在排隊?
+            //查詢是否有用戶正在排隊?
             //有:進入 無:不進入
-            if (findLineUser() != null){
-                //透過會話id取得客戶資訊
+            if (findLineUser() != null) {
+                //透過會話id取得用戶資訊
                 SocketUserInfo userInfo = userSessionMap.get(findLineUser());
-                //將客戶的目標會話id綁定(將客戶綁定客服)
+                //將用戶的目標會話id綁定(將用戶綁定客服)
                 userInfo.setTargetSessionId(serverInfo.getSessionId());
-                //將客服的目標會話id綁定(將客服綁定客戶)
+                //將客服的目標會話id綁定(將客服綁定用戶)
                 serverInfo.setTargetSessionId(userInfo.getSessionId());
-                //因為找到客服所以更新在線"客戶"
+                //因為找到客服所以更新在線"用戶"
                 userSessionMap.put(userInfo.getSessionId(), userInfo);
 
-               //System.out.println("客服"+ serverInfo.getSessionId() + "正在為用戶" + userInfo.getSessionId() + "服務");
-
-//                Map<String, String> msg = new HashMap<>();
                 //客服顯示在為誰服務
-                msg.put("msg", "正在為用戶"+userInfo.getSessionId()+"服務！");
+                msg.put("msg", "正在為用戶" + userInfo.getSessionId() + "服務！");
                 sendMsg(serverInfo.getSession(), JSON.toJSONString(msg));
 
-                //客戶顯示誰在為他服務
-                msg.put("msg", "客服"+serverInfo.getSessionId()+"正在為您服務！");
+                //用戶顯示誰在為他服務
+                msg.put("msg", "客服" + serverInfo.getName() + "正在為您服務！");
                 sendMsg(userInfo.getSession(), JSON.toJSONString(msg));
-
 
             }
             //無論是否有連接會員都放進"在線客服",並提示客服上線成功
             serverSessionMap.put(session.getId(), serverInfo);
-            //System.out.println("客服：" + serverInfo.getSessionId() + "連線成功,目前客服數量:" + serverSessionMap.size());
         }
 
 
-        //當客戶進入聊天室
-        if (role.equals(1)) {
-            //創建一個客戶身分
+        //當用戶進入聊天室
+        if (role == 1) {
+            //創建一個用戶身分
             SocketUserInfo userInfo = new SocketUserInfo();
             userInfo.setSessionId(session.getId());
             userInfo.setSession(session);
-            userInfo.setUserRole("客戶");
-
-            //告訴客戶連線狀態為成功
-            state.put("state", "success");
+            userInfo.setUserRole("用戶");
 
             //查詢是否有連線到閒置客服
-            if (findFreeServer() != null){
+            if (findFreeServer() != null) {
                 //透過SessionID找到閒置的客服
                 SocketUserInfo serverInfo = serverSessionMap.get(findFreeServer());
-                //將客服的目標會話id綁定(客服綁定客戶)
+                //將客服的目標會話id綁定(客服綁定用戶)
                 serverInfo.setTargetSessionId(userInfo.getSessionId());
                 //更新該客服狀態(有綁定目標)
                 serverSessionMap.put(serverInfo.getSessionId(), serverInfo);
-                //將客戶的目標會話id綁定(客戶綁定客服)
+                //將用戶的目標會話id綁定(用戶綁定客服)
                 userInfo.setTargetSessionId(serverInfo.getSessionId());
                 //System.out.println("客服"+ serverInfo.getSessionId() + "正在為" + userInfo.getSessionId()+"服務");
 
                 //客服顯示為誰服務
-                msg.put("msg", "正在為用戶"+userInfo.getSessionId()+"服務！");
+                msg.put("msg", "正在為用戶" + userInfo.getSessionId() + "服務！");
                 sendMsg(serverInfo.getSession(), JSON.toJSONString(msg));
 
-                //客戶顯示誰在服務
-                msg.put("msg", "客服" + serverInfo.getSessionId() + "正在為您服務！");
+                //用戶顯示誰在服務
+                msg.put("msg", "客服人員:" + serverInfo.getName() + "正在為您服務！");
                 sendMsg(userInfo.getSession(), JSON.toJSONString(msg));
 
             }
             //沒有連線到客服人員
             else {
-                state.put("msg", "目前客服人員忙線中！正在尋找客服人員");
-                sendMsg(userInfo.getSession(), JSON.toJSONString(state));
+                msg.put("msg", "目前客服人員忙線中！請繼續等待客服人員！");
+                sendMsg(userInfo.getSession(), JSON.toJSONString(msg));
             }
 
-            //不管客戶是否有找到客服都要把它放進已上線的客戶
+            //不管用戶是否有找到客服都要把它放進已上線的用戶
             userSessionMap.put(session.getId(), userInfo);
             //System.out.println("用戶編號：" + userInfo.getSessionId() + "伺服器在線用戶數量為:" + userSessionMap.size());
         }
-        //告訴客人訊息
-//        String result = JSON.toJSONString(state);
-//        //System.out.println(result);
-//        sendMsg(session, result);
     }
 
     //關閉連線
     @OnClose
-    public void onClose(Session session) {
-        //取得客服資訊
+    public void onClose(@PathParam(value = "role") int role, Session session) {
+
         SocketUserInfo serverInfo = serverSessionMap.get(session.getId());
-        //檢查客服資訊是否不為空
-        if (serverInfo != null) {
+        Map<String, String> msg = new HashMap<>();
+        //客服取消連線
+        if (role == 0) {
             //將該客服從在線客服的map中移除
             serverSessionMap.remove(session.getId());
             //檢查該客服是否有正在服務的用戶
-            if (null != serverInfo.getTargetSessionId()){
+            if (null != serverInfo.getTargetSessionId()) {
                 //向該用戶發送訊息
-                Map<String, String> result = new HashMap<>();
-                result.put("msg", "網路異常,請重新整理！");
-                sendMsg(userSessionMap.get(serverInfo.getTargetSessionId()).getSession(), JSON.toJSONString(result));
+                msg.put("msg", "您的問題已解決！謝謝您的支持！");
+                sendMsg(userSessionMap.get(serverInfo.getTargetSessionId()).getSession(), JSON.toJSONString(msg));
             }
-            //System.out.println("客服編號：" + serverInfo.getSessionId() + "退出連線,目前客服上線數量:" + serverSessionMap.size());
-        } else {
+        }
+        //用戶取消連線
+        else {
             //將該用戶從在線用戶的map中移除
             userSessionMap.remove(session.getId());
+
             //從客服中解除該用戶的綁定
-            for (SocketUserInfo serverSocketInfo: serverSessionMap.values()) {
+            for (SocketUserInfo serverSocketInfo : serverSessionMap.values()) {
+
+                msg.put("msg", "用戶的問題已得到解決！為您轉接其他用戶！");
+                sendMsg(serverSocketInfo.getSession(), JSON.toJSONString(msg));
+
                 //查找綁定的客服，即客服綁定的用戶不為空，並且綁定的用戶id與現在下線的用戶id一致
-                if (serverSocketInfo.getTargetSessionId() != null && serverSocketInfo.getTargetSessionId().equals(session.getId())){
+                if (serverSocketInfo.getTargetSessionId() != null && serverSocketInfo.getTargetSessionId().equals(session.getId())) {
                     //解除綁定
                     serverSocketInfo.setTargetSessionId(null);
                     serverSessionMap.put(serverSocketInfo.getSessionId(), serverSocketInfo);
-                    //System.out.println("用戶編號：" + session.getId() + "斷開了與客服" + serverSocketInfo.getSessionId() + "的連接");
 
                     //客服解除綁定後，可能還會有在線排隊的用戶，就讓這個客服去服務
                     String lineUser = findLineUser();
-                    if (lineUser != null){
+                    if (lineUser != null) {
                         //將用戶綁定到該客服
                         serverSocketInfo.setTargetSessionId(lineUser);
                         serverSessionMap.put(serverSocketInfo.getSessionId(), serverSocketInfo);
                         //將該客服綁定到用戶
                         userSessionMap.get(lineUser).setTargetSessionId(serverSocketInfo.getSessionId());
-                        //System.out.println("客服"+ serverSocketInfo.getSessionId() + "正在為" + lineUser+"服務");
 
-                        Map<String, String> result = new HashMap<>();
                         //客服顯示用戶資訊
-                        result.put("msg", "正在為用戶"+lineUser+"服務！");
-                        sendMsg(serverSocketInfo.getSession(), JSON.toJSONString(result));
+                        msg.put("msg", "正在為用戶" + lineUser + "服務！");
+                        sendMsg(serverSocketInfo.getSession(), JSON.toJSONString(msg));
                         //用戶顯示客服資訊
-                        result.put("msg", "客服"+serverSocketInfo.getSessionId()+"正在為您服務！");
-                        sendMsg(userSessionMap.get(lineUser).getSession(), JSON.toJSONString(result));
+                        msg.put("msg", "客服" + serverSocketInfo.getName() + "正在為您服務！");
+                        sendMsg(userSessionMap.get(lineUser).getSession(), JSON.toJSONString(msg));
                     }
                 }
             }
-            //System.out.println("用戶編號：" + session.getId() + "退出了連接，當前在線用戶共計：" + userSessionMap.size());
         }
     }
 
-
     // 定義一個方法處理用戶和客服端互相傳遞的消息
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message,@PathParam(value = "role") int role, Session session) {
         // 創建一個映射來存儲消息
-        Map<String, String> result = new HashMap<>();
+        Map<String, String> msg = new HashMap<>();
 
-        // 從在線客服的映射中獲取當前會話的客服資訊
-        SocketUserInfo serverInfo = serverSessionMap.get(session.getId());
-
-        // 如果客服資訊不為空，則處理客服的消息
-        if (serverInfo != null) {
-            // 打印客服發送的消息和目標用戶的ID
-            //System.out.println("客服"+ session.getId()+"發送消息：\""+ message +"\"給用戶"+serverSessionMap.get(session.getId()).getTargetSessionId());
-
-            // 將客服的ID和消息添加到結果映射中
-            result.put("msg", "客服"+session.getId()+"："+message);
-
+        //客服傳訊息給用戶
+        if (role == 0) {
+            // 從在線客服的映射中獲取當前會話的客服資訊
+            SocketUserInfo serverInfo = serverSessionMap.get(session.getId());
+            msg.put("msg", "客服人員" + serverInfo.getName() + "：" + message);
             // 判斷客服是否有服務的用戶，如果有，則將消息發送給該用戶
-            if (null != serverSessionMap.get(session.getId()).getTargetSessionId()){
-                sendMsg(userSessionMap.get(serverSessionMap.get(session.getId()).getTargetSessionId()).getSession(), JSON.toJSONString(result));
+            if (null != serverSessionMap.get(session.getId()).getTargetSessionId()) {
+                sendMsg(userSessionMap.get(serverInfo.getTargetSessionId()).getSession(), JSON.toJSONString(msg));
             }
-
-        } else { // 如果客服資訊為空，則處理用戶的消息
-            // 打印用戶發送的消息和目標客服的ID
-            //System.out.println("用戶"+ session.getId()+"發送消息：\""+ message +"\"給客服"+userSessionMap.get(session.getId()).getTargetSessionId());
-
+        }
+        //處理用戶傳訊息給客服
+        else {
             // 將用戶的ID和消息添加到結果映射中
-            result.put("msg", "用戶"+session.getId()+"："+message);
+            msg.put("msg", "用戶" + session.getId() + "：" + message);
 
+            SocketUserInfo userInfo = userSessionMap.get(session.getId());
             // 從上線中用戶找到該用戶並找到他的目標id不為null時發送訊息
-            if (userSessionMap.get(session.getId()).getTargetSessionId() != null){
-                sendMsg(serverSessionMap.get(userSessionMap.get(session.getId()).getTargetSessionId()).getSession(), JSON.toJSONString(result));
+            if (userSessionMap.get(session.getId()).getTargetSessionId() != null) {
+                sendMsg(serverSessionMap.get(userInfo.getTargetSessionId()).getSession(), JSON.toJSONString(msg));
             }
         }
     }
@@ -235,13 +214,13 @@ public class SocketController {
     }
 
     // 定義一個同步方法，用於尋找閒置的用戶
-    private synchronized String findLineUser(){
+    private synchronized String findLineUser() {
         // 檢查在線用戶的map是否有元素
-        if (userSessionMap.size() > 0){
+        if (userSessionMap.size() > 0) {
             // 遍歷在線用戶的map
-            for (SocketUserInfo UserInfo: userSessionMap.values()) {
+            for (SocketUserInfo UserInfo : userSessionMap.values()) {
                 // 檢查用戶是否沒有被客服服務，即目標會話ID是否為空
-                if (null == UserInfo.getTargetSessionId()){
+                if (null == UserInfo.getTargetSessionId()) {
                     // 如果是，返回該用戶的會話ID
                     return UserInfo.getSessionId();
                 }
@@ -252,13 +231,13 @@ public class SocketController {
     }
 
     // 定義一個同步方法，用於尋找閒置的客服
-    private synchronized String findFreeServer(){
+    private synchronized String findFreeServer() {
         // 檢查在線客服的map是否有元素
-        if (serverSessionMap.size() > 0){
+        if (serverSessionMap.size() > 0) {
             // 遍歷在線客服的map
-            for (SocketUserInfo serverInfo: serverSessionMap.values()) {
+            for (SocketUserInfo serverInfo : serverSessionMap.values()) {
                 // 檢查客服是否沒有在服務用戶，即目標會話ID是否為空
-                if (null == serverInfo.getTargetSessionId()){
+                if (null == serverInfo.getTargetSessionId()) {
                     // 如果是，返回該客服的會話ID
                     return serverInfo.getSessionId();
                 }
