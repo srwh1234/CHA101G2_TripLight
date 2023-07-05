8396b40b2fc053b4dff509e2c9cf0fe9e4f088eb
Teim <Teieml8090@gmail.com>
2023-07-05 15:29:48 +0800

    update

--- src/main/java/com/tw/contact/privateChat/ChatRecordRepository.java
+++ src/main/java/com/tw/contact/privateChat/ChatRecordRepository.java
@@ -1,8 +1,8 @@
-package com.tw.contact.privateChat;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRecordRepository extends MongoRepository<ChatRecord, String> {
-}
+//package com.tw.contact.privateChat;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ChatRecordRepository extends MongoRepository<ChatRecord, String> {
+//}
