216e3d048f469f7e75acd1ff2b88d065f52b1cc5
srwh1234 <srwh1234@msn.com>
2023-07-22 14:23:49 +0800

    測試一下

--- src/main/java/com/tw/ticket/TestCommandLineRunner.java
+++ src/main/java/com/tw/ticket/TestCommandLineRunner.java
@@ -5,30 +5,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 @Component
 public class TestCommandLineRunner implements CommandLineRunner {
-	private static final Logger log = LoggerFactory.getLogger(TestCommandLineRunner.class);
+    private static final Logger log = LoggerFactory.getLogger(TestCommandLineRunner.class);
 
-	@Autowired
-	private Config config;
+    @Autowired
+    private Config config;
 
-	@Autowired
-	private JedisPool jedisPool;
+    @Autowired
+    private JedisPool jedisPool;
 
-	@Override
-	public void run(final String... args) {
+    @Override
+    public void run(final String... args) {
+        //測試一下
 
-		// 判斷redis伺服器有無開啟
-		try (Jedis jedis = jedisPool.getResource()) {
-			config.setRedisServerStarted(true);
-		} catch (final Exception e) {
-			config.setRedisServerStarted(false);
-		} finally {
-			log.info("Redis Server Started:" + config.isRedisServerStarted());
-		}
-	}
+
+        // 判斷redis伺服器有無開啟
+        try (final Jedis jedis = jedisPool.getResource()) {
+            config.setRedisServerStarted(true);
+        } catch (final Exception e) {
+            config.setRedisServerStarted(false);
+        } finally {
+            log.info("Redis Server Started:" + config.isRedisServerStarted());
+        }
+    }
 }
